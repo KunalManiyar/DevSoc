7b43514fb18f2341fb66081a43a0b78e49658b8f
TheUnknownBuilder <vani.kamani@somaiya.edu>
2022-03-21 11:12:59 +0530

    temporary changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,63 @@
-// import React, { Fragment, useState} from 'react';
-// import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
-// import Navbar from './components/layout/Navbar';
-// import Landing from './components/layout/Landing';
-// import Register from './components/auth/Register';
-// import Login from './components/auth/Login';
-// //import { useRoutes } from 'hookrouter';
-// //import Routes from './router';
-// import './App.css';
-
-// //const App = ()=> (
-//   // function App(){
-//   //   const routeResult = useRoutes(Routes)
-//   // return routeResult
-
-//   // }
-// //   const showRegister = () =>
-// //   {
-// //     if(window.location.pathname === "/"){
-// //       return <Register />
-// //     }
-// //   }
-// // export default () =>
-// // {
-// //   return <div className="ui container">
-// //   {showRegister()}
-// //   </div>
-// // };
-// const App = ()=> (
-//   <Fragment>
-//     <Register />
-//   </Fragment>
-  
-// )
-// export default App;
-
-
-import React, { Fragment } from 'react';
+import React, { Fragment, useState} from 'react';
 import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
-
+//import { useRoutes } from 'hookrouter';
+//import Routes from './router';
 import './App.css';
 
+//const App = ()=> (
+  // function App(){
+  //   const routeResult = useRoutes(Routes)
+  // return routeResult
+
+  // }
+//   const showRegister = () =>
+//   {
+//     if(window.location.pathname === "/"){
+//       return <Register />
+//     }
+//   }
+// export default () =>
+// {
+//   return <div className="ui container">
+//   {showRegister()}
+//   </div>
+// };
 const App = ()=> (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path='/' component={ Landing } />
-      <section className='container'>
-        <Routes>
-        <Route exact path='/register' component={Register} />
-        <Route exact path='/login' component={Login} />
-        </Routes>
-      </section>
-    </Fragment>
-  </Router>
+  <Fragment>
+    <Navbar />
+    <Register />
+  </Fragment>
   
 )
 export default App;
+
+
+// import React, { Fragment } from 'react';
+// import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
+// import Navbar from './components/layout/Navbar';
+// import Landing from './components/layout/Landing';
+// import Register from './components/auth/Register';
+// import Login from './components/auth/Login';
+
+// import './App.css';
+
+// const App = ()=> (
+//   <Router>
+//     <Fragment>
+//       <Navbar />
+//       <Route exact path='/' component={ Landing } />
+//       <section className='container'>
+//         <Routes>
+//         <Route exact path='/register' component={Register} />
+//         <Route exact path='/login' component={Login} />
+//         </Routes>
+//       </section>
+//     </Fragment>
+//   </Router>
+  
+// )
+// export default App;
